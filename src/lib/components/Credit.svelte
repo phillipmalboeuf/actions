97633b226fa7145c69517720ba254a55aa3b7683
Phillip Malboeuf <phil@phils.computer>
2024-07-26 13:05:21 -0400

    dimensions

--- src/lib/components/Credit.svelte
+++ src/lib/components/Credit.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { TypeOeuvreSkeleton } from '$lib/clients/content_types'
   import type { Entry } from 'contentful'
+  import { intlNumber } from '$lib/formatters'
 
   export let oeuvre: Entry<TypeOeuvreSkeleton, "WITHOUT_UNRESOLVABLE_LINKS">
   export let full = false
@@ -11,4 +12,5 @@
 {#if full}
 <div>{oeuvre.fields.anneeDeRealisation || oeuvre.fields.annee}</div>
 <div>{oeuvre.fields.medium}</div>
+<div>{intlNumber(oeuvre.fields.largeur)} cm x {intlNumber(oeuvre.fields.hauteur)} cm {#if oeuvre.fields.profondeur}x {intlNumber(oeuvre.fields.profondeur)} cm{/if}</div>
 {/if}
