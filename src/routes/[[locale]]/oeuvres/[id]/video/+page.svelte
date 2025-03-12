3a2b169895481d6db9b1166bb49138dbdd41946e
Phillip Malboeuf <phil@phils.computer>
2025-03-12 12:22:34 -0400

    video transcription

--- src/routes/[[locale]]/oeuvres/[id]/video/+page.svelte
+++ src/routes/[[locale]]/oeuvres/[id]/video/+page.svelte
@@ -4,7 +4,9 @@
   import { page } from '$app/stores'
   import { browser } from '$app/environment'
   import type { MouseEventHandler } from 'svelte/elements'
+
   import Icon from '$lib/components/Icon.svelte'
+  import Document from '$lib/components/document/index.svelte'
 
   import type { PageData } from './$types' 
   export let data: PageData
@@ -36,6 +38,22 @@
       <p>{data.oeuvre.fields.droits}</p>
       {/if}
     </small>
+
+    <input type="checkbox" name="transcription" id="transcription" on:input={(e) => {
+    e.currentTarget.blur()
+    }} />
+    <label for="transcription">
+      <span class="back"><Icon i="back" label="Fermer" /></span>
+      <span class="button">transcription</span>
+    </label>
+
+    <aside>
+      {#if data.oeuvre.fields.transcription}
+      <Document body={data.oeuvre.fields.transcription} />
+      {:else}
+      <p>Aucune transcription disponible pour cette vidéo.</p>
+      {/if}
+    </aside>
   </div>
 </dialog>
 
@@ -83,6 +101,75 @@
       @media (max-width: $mobile) {
         left: ($mobile_gap * 1);
       }
+    }
+
+    input[name="transcription"] {
+      display: none;
+
+      & + label {
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: calc(90vw);
+        max-width: 420px;
+        padding: $gap calc($gap * 2);
+        z-index: 4001;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        gap: $gap;
+
+        .back {
+          display: none;
+          transition: padding-left 0.333s;
+
+          &:hover {
+            padding-left: $gap;
+          }
+        }
+      }
+
+      &:checked + label {
+        justify-content: space-between;
+
+        .button {
+          color: $black;
+          border-color: transparent;
+          pointer-events: none;
+        }
+
+        .back {
+          display: inline-block;
+          color: $black;
+          cursor: pointer;
+        }
+      }
+
+      &:checked ~ aside {
+        transform: translateX(0);
+      }
+    }
+
+    aside {
+      padding: calc($gap * 2);
+      padding-top: calc($gap * 4);
+      background-color: $beige-dark;
+      color: $black;
+      border-radius: $base;
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+      
+      position: fixed;
+      top: 0;
+      right: 0;
+      z-index: 4000;
+      width: calc(90vw);
+      max-width: 420px;
+      height: 100lvh;
+      overflow-y: auto;
+
+      transition: transform 0.333s;
+      transform: translateX(100%);
     }
   }
 </style>
